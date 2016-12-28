49bcd0486b1ab03478d7c817970a1cfae1531c51
unknown <MRuser@BLR-KRM-MR20-10.NIITEDUCATION.COM>
2016-12-28 13:04:12 +0530

    first update

---
+++ anil.java
@@ -0,0 +1,10 @@
+class anil{
+static int a=40;
+static float b=40.3;
+{
+System.out.println(a+b);
+}
+public static void main(String[]args){
+
+}
+}
---
+++ sudeep.java
@@ -0,0 +1,11 @@
+class sudeep{
+
+static int a=30;
+static int b=40;
+static
+{
+System.out.println(a+b);
+}
+public static void main(String[]args){
+}
+}
