e543ad501c921003ea32e6fc124ab69cd27e44af
neradala <harikrishna.neradala426@gmail.com>
2017-01-02 16:12:06 +0530

    Create B.java

---
+++ overloading/B.java
@@ -0,0 +1,19 @@
+package com.hari;
+
+public class B {
+public static void main(String[]args){	
+  B obj= new B();
+  obj.krishna(22,33);
+  obj.krishna(44,55,66);
+}
+void krishna(int a,int b)
+{
+	System.out.println(a+b);
+}
+
+void krishna(int e,int c,int d)
+
+{
+	System.out.println(e+c+d);
+}
+}
