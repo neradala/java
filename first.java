4b28a935034f9edc417acdeed5635b00ff15d02b
neradala <harikrishna.neradala426@gmail.com>
2017-01-13 12:02:03 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,23 @@
+package com.excetionhandling;
+
+public class first {
+	
+	public static void main(String[]arg){
+		
+		{
+	int array[]={2,34,5,6,};
+	
+	int a=array[2];
+			
+			System.out.println( a);
+		}
+		
+	}
+}
+	
+	
+
+	
+	
+
+
---
+++ kit.java
@@ -0,0 +1,46 @@
+package com.excetionhandling;
+
+public class kit{
+	
+public static void main(String[]args){
+	
+	
+       
+        try{     		
+		
+			
+			
+			
+			int a=44/4;
+			System.out.println(  a);
+			
+		}catch(NullPointerException e)
+        
+        {
+			System.out.println(e);
+        }
+        
+        finally{
+        	System.out.println("print o");
+        	
+        }
+        
+        {
+        	System.out.println("print p");
+        	
+        }
+       
+}
+}
+
+				
+				
+				
+			
+		
+	
+	
+	
+	
+	
+
